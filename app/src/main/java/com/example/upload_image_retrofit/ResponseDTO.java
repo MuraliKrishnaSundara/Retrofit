807cc179d1c34706a9ecb05defbbf75474064e33
MuraliKrishnaSundara <Murali.Sundara@ssipmt.com>
2021-07-28 18:56:40 +0530

    Upload Image Retrofit

---
+++ app/src/main/java/com/example/upload_image_retrofit/ResponseDTO.java
@@ -0,0 +1,29 @@
+package com.example.upload_image_retrofit;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class ResponseDTO implements Serializable {
+
+    @SerializedName("data")
+    private DataDTO data;
+
+    @SerializedName("success")
+    private boolean success;
+
+    @SerializedName("status")
+    private int status;
+
+    public DataDTO getData() {
+        return data;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
